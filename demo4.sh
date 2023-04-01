7b09afc4d05c56c78840e8c8b9ee2364004f5486
openstack-rap <128293768+openstack-rap@users.noreply.github.com>
2023-04-01 11:25:55 +0530

    Update and rename demo3.sh to demo4.sh

--- demo4.sh
+++ demo4.sh
@@ -2,9 +2,10 @@
 echo "hii"
 echo "before"
 pwd
-source /snap/demo3/x1/tools/two.sh
+source /snap/demo4/x1/tools/two.sh
 echo "before fun"
 abc
 echo "after fun"
 sudo apt install net-tools
 echo "done"
+echo "$LANG"
